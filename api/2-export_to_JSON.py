53c697231c8af3e0219b51aeb59aec5671dd29f8
mrktptn <marktipton95@gmail.com>
2023-11-16 17:22:24 -0600

    added ability to export to json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -5,6 +5,7 @@ returns information about his/her TODO list progress.
 extend previous script to export data in the JSON format.
 Records all tasks owned by an employee
 """
+import json
 import requests
 import sys
 
@@ -36,24 +37,34 @@ def gather_data():
     user_id_index = user_id + 1
     employee_counts[user_id_index] = {"completed": 0, "total": 0}
 
+    tasks = []
+
     for todo in todos_data:
         if user_id_index == todo["userId"]:
+            task_info = {
+                "task": todo["title"],
+                "completed": todo["completed"],
+                "username": employee_name
+            }
+            tasks.append(task_info)
 
-            employee_counts[user_id_index]["total"] += 1
+    # comp_tasks = employee_counts[user_id_index]["completed"]
+    # total = employee_counts[user_id_index]["total"]
+    # # print first line
+    # print(
+    #     f'Employee {employee_name} is done with tasks({comp_tasks}/{total}):'
+    # )
 
-            if todo["completed"]:
-                employee_counts[user_id_index]["completed"] += 1
+    # for todo in todos_data:
+    #     if todo["completed"] and user_id_index == todo["userId"]:
+    #         print(f'\t {todo["title"]}')
 
-    comp_tasks = employee_counts[user_id_index]["completed"]
-    total = employee_counts[user_id_index]["total"]
-    # print first line
-    print(
-        f'Employee {employee_name} is done with tasks({comp_tasks}/{total}):'
-    )
+    json_data = {str(user_id_index): tasks}
 
-    for todo in todos_data:
-        if todo["completed"] and user_id_index == todo["userId"]:
-            print(f'\t {todo["title"]}')
+    filename = f"{user_id_index}.json"
+
+    with open(filename, 'w') as json_file:
+        json.dump(json_data, json_file, indent=2)
 
 
 if __name__ == "__main__":
