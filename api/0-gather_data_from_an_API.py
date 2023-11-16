231bce1aa33b07ab801a23276c118a2498eac759
mrktptn <marktipton95@gmail.com>
2023-11-16 12:19:54 -0600

    using requests module to gather data from api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,3 +3,20 @@
 using a sample REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
+import requests
+
+get_employee = requests.get("https://jsonplaceholder.typicode.com/users")
+get_todos = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+print(get_employee.status_code)
+print(get_employee.json())
+
+print(get_todos.status_code)
+print(get_todos.json())
+
+employee = get_employee.json()[0]['name']
+
+comp_tasks = get_todos.json()['completed']
+
+tot_tasks = get_todos.json()['title']
+
+print(f'Employee {employee} is done with tasks({comp_tasks}/{tot_tasks})')
